15da634639b5f1517d3df8a3ea296fffc421f347
michael <dantasmichael9@gmail.com>
2026-03-30 13:36:33 -0300

    Ajustes no siteo host

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 4173,
   },
   preview: {
-    allowedHosts: ['aemconsulting.com.br'], // libera seu domínio DDNS
+    allowedHosts: ['www.aemconsulting.com.br'], // libera seu domínio DDNS
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
